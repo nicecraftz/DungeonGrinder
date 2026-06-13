e94e867c29ff342d87df3e8361f45f14708c1914
Alessandro C. <Alessandromaria.calista@gmail.com>
2026-06-13 11:31:27 +0200

    fix: modificata la velocità del giocatore

--- src/main/java/it/unicam/cs/mpgc/rpg129874/dungeongrinder/domain/entity/player/Player.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129874/dungeongrinder/domain/entity/player/Player.java
@@ -5,7 +5,7 @@ import it.unicam.cs.mpgc.rpg129874.dungeongrinder.domain.entity.compound.EntityD
 import it.unicam.cs.mpgc.rpg129874.dungeongrinder.domain.world.position.Position;
 
 public class Player extends HumanEntity {
-    public static final EntityAttributes PLAYER_ATTRIBUTES = new EntityAttributes(100, 8, 3);
+    public static final EntityAttributes PLAYER_ATTRIBUTES = new EntityAttributes(100, 4, 3);
     public static final EntityDescriptor PLAYER_DESCRIPTOR = new EntityDescriptor("Giocatore", PLAYER_ATTRIBUTES);
 
     public Player() {
